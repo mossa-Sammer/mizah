07d11e339bc66136c70996341f0bde6ac3581f26
fadeomar <fadeomar2015@gmail.com>
2020-08-09 20:34:33 +0300

    add the header to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 
+// sections
+import OurStorySection from './components/OurStorySection';
+
 // components
-import Waves from './components/Waves';
-import ActiveWave from './components/SVG/ActiveWave';
 import SliderContainer from './components/SliderContainer';
 import IconsSection from './components/IconsSection';
+import Header from './components/Header';
 
 import 'antd/dist/antd.css';
 import './App.css';
 
 function App() {
   const [slide, setSlide] = useState('left');
+  const [lang, setLang] = useState('en');
   return (
     <div className="App">
-      <SliderContainer slide={slide} setSlide={setSlide}/>  
-      <section style={{width: '100%', height: '100vh'}}></section>
-      <IconsSection slide={slide}/> 
+      <Header lang={lang} setLang={setLang} />
+      <SliderContainer slide={slide} setSlide={setSlide} />
+      <section style={{ width: '100%', height: '100vh', backgroundColor: 'gray' }} />
+      <OurStorySection />
+      <IconsSection slide={slide} />
     </div>
   );
 }
