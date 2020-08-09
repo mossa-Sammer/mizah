32bdd9718efc7b6321e2ce1de96e05694a8924fe
fadeomar <fadeomar2015@gmail.com>
2020-08-09 20:32:15 +0300

    add animation for the icons

--- client/src/components/IconsSection/styled.js
+++ client/src/components/IconsSection/styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { rotate,wobble, rubberBand, tada } from './keyframes';
+import { rotate, wobble, rubberBand, tada, swing } from './keyframes';
+
 export const Content = styled.div`
   width: 200%;
   height: 300px;
@@ -12,31 +13,29 @@ export const Content = styled.div`
 `;
 
 export const LeftElement = styled.div`
-width: 300px;
-height: 300px;
-position: fixed;
-left: 250px;
-top: 0;
+  width: 300px;
+  height: 300px;
+  position: fixed;
+  left: 250px;
+  top: 0;
 `;
 
 export const RightElement = styled.div`
-width: 300px;
-height: 300px;
-position: fixed;
-right: 250px;
-top: 0;
+  width: 300px;
+  height: 300px;
+  position: fixed;
+  right: 250px;
+  top: 0;
 `;
 
 export const FirstLeftIconContainer = styled.div`
-  width: 50px;
-  height: 50px;
+  width: 70px;
+  height: 70px;
   position: absolute;
-  top: 0;
-  left: 0;
-  transform: ${({y}) => y !== 0 ?
-   y >= -320 ? `translate(${y + 150}px, ${y + 185}px)`
-   :`translate(${y + 150}px, -135px)`
-   : null};
+  // top: 0;
+  // left: -100px;
+  left: ${({ y }) => (y <= -220 ? (y >= -580 ? `${y + 100}px` : '-500px') : '-100px')};
+  top: ${({ y }) => (y <= -220 ? (y >= -290 ? `${y + 190}px` : '-90px') : '-30px')};
 `;
 
 export const SecondLeftIconContainer = styled.div`
@@ -45,10 +44,12 @@ export const SecondLeftIconContainer = styled.div`
   position: absolute;
   top: 0;
   right: 0;
-  transform: ${({y}) => y ?
-  y >= -280 ? `translate(${y*-1 - 150 }px, ${y + 190}px)`
-  :`translate(${y > -700 ? -1 * y - 150 : 700}px, -100px)`
-  : null};
+  transform: ${({ y }) =>
+    y <= -220
+      ? y >= -280
+        ? `translate(${y * -1 - 150}px, ${y + 190}px)`
+        : `translate(${y > -700 ? -1 * y - 150 : 700}px, -100px)`
+      : null};
   // transition: 0.5s;
 `;
 
@@ -69,28 +70,41 @@ export const FourthLeftIconContainer = styled.div`
 `;
 
 export const Icon1 = styled.img`
-width: 100px;
-height: 130px;
-position: absolute;
-top: -50px;
-left: -50px;
-animation: ${wobble} 2s linear infinite;
+  width: 100px;
+  height: 130px;
+  position: absolute;
+  top: -50px;
+  left: -50px;
+  animation: ${wobble} 2s linear infinite;
 `;
 
 export const Icon2 = styled.img`
-width: 100px;
-height: 100px;
-position: absolute;
-top: -40px;
-left: -50px;
-animation: ${rotate} 2s linear infinite;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: -40px;
+  left: -50px;
+  animation: ${rotate} 2s linear infinite;
 `;
 
 export const Icon3 = styled.img`
-width: 100px;
-height: 100px;
-position: absolute;
-top: -40px;
-left: -50px;
-animation: ${tada} 2s linear infinite;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: -40px;
+  left: -50px;
+  animation: ${tada} 2s linear infinite;
+`;
+
+export const ImgContainer = styled.div`
+  width: 320px;
+  height: 320px;
+  position: absolute;
+  left: -80px;
+  animation: ${swing} 5s linear infinite;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  height: 100%;
 `;
