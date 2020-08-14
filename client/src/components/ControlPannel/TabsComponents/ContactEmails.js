456476e732f2f52d0b6c8fbd05f5a9abc3228988
fadeomar <fadeomar2015@gmail.com>
2020-08-14 17:44:04 +0300

    create Contact emails page

--- client/src/components/ControlPannel/TabsComponents/ContactEmails.js
+++ client/src/components/ControlPannel/TabsComponents/ContactEmails.js
@@ -1,16 +1,52 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core';
 
+import { TitleContainer } from './styled';
+
+import Table from '../../Table';
+import { RemoveRejoinCol } from '../../Table/tableSharedData';
+
+const rowClick = () => console.log('Row has been clicked');
+
 const style = {
   title: {
-    color: 'green',
+    color: '#8368af',
+    textAlign: 'left',
+    paddingLeft: '20px',
+    marginBottom: 30,
   },
 };
 
+export const earnGroupMembersData = [
+  {
+    name: 'fadi oamr',
+    email: 'fadeomar2015@gmail.com',
+    phoneNo: '21341465',
+    body: 'sasd asd asd asa',
+  },
+];
+
 const ContactEmails = ({ classes }) => {
   return (
-    <div>
-      <h1 className={classes.title}>Hi from contact Emails</h1>
+    <div style={{ paddingLeft: 320 }}>
+      <TitleContainer>
+        <h1 className={classes.title}>Contact Emails Page :</h1>
+      </TitleContainer>
+
+      <Table
+        hideSearch
+        color="blue"
+        data={earnGroupMembersData}
+        showPagination={false}
+        columns={[
+          { title: 'Name', field: 'name' },
+          { title: 'Email', field: 'email' },
+          { title: 'Phone No', field: 'phoneNo' },
+          { title: 'Body', field: 'body' },
+          RemoveRejoinCol,
+        ]}
+        onRowClick={rowClick}
+      />
     </div>
   );
 };
