7c8d09e4fcc1852fbafad4b2df438fa0d8b106b7
fadeomar <fadeomar2015@gmail.com>
2020-08-14 17:53:59 +0300

    add log out button to the header

--- client/src/components/ControlPannel/Header.js
+++ client/src/components/ControlPannel/Header.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { AppBar, Toolbar, Grid, InputBase, IconButton, Badge, makeStyles } from '@material-ui/core';
-// import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
-// import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-// import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
-// import SearchIcon from '@material-ui/icons/Search';
+import { AppBar, Toolbar, Grid, makeStyles, Button } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
   },
   searchInput: {
     opacity: '0.6',
@@ -28,24 +24,10 @@ export default function Header() {
   return (
     <AppBar position="static" className={classes.root}>
       <Toolbar>
-        <Grid container alignItems="center">
-          <Grid item>
-            <InputBase placeholder="Search topics" className={classes.searchInput} />
-          </Grid>
-          <Grid item sm />
-          <Grid item>
-            <IconButton>
-              <Badge badgeContent={4} color="secondary">
-                {/* <NotificationsNoneIcon fontSize="small" /> */}
-              </Badge>
-            </IconButton>
-            <IconButton>
-              <Badge badgeContent={3} color="primary">
-                {/* <ChatBubbleOutlineIcon fontSize="small" /> */}
-              </Badge>
-            </IconButton>
-            <IconButton>{/* <PowerSettingsNewIcon fontSize="small" /> */}</IconButton>
-          </Grid>
+        <Grid container xs={12} justify="flex-end">
+          <Button variant="contained" color="secondary" onClick={() => {}}>
+            Log out
+          </Button>
         </Grid>
       </Toolbar>
     </AppBar>
