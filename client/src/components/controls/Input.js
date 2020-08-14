786719771d464dd1804c5ab06b02f8e1a855df7f
fadeomar <fadeomar2015@gmail.com>
2020-08-14 14:43:35 +0300

    creat custopm Inputs

---
+++ client/src/components/controls/Input.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { TextField } from '@material-ui/core';
+
+export default function Input(props) {
+  const { name, label, value, error = null, onChange, multiline } = props;
+  return (
+    <TextField
+      variant="outlined"
+      label={label}
+      name={name}
+      value={value}
+      onChange={onChange}
+      multiline={multiline}
+      rows={multiline && 4}
+      {...(error && { error: true, helperText: error })}
+      style={{ marginBottom: 30 }}
+    />
+  );
+}
