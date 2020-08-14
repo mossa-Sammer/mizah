d59ca1b16887df19f3cd8a596049a79f7aeb7f27
fadeomar <fadeomar2015@gmail.com>
2020-08-14 14:37:41 +0300

    add styles for the tab component

---
+++ client/src/components/ControlPannel/TabsComponents/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const BtnContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 50px;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
