8e8e0b1a3fc1c684f958f01111d85a083cd84ecd
fadeomar <fadeomar2015@gmail.com>
2020-08-15 13:19:16 +0300

    pass the lang to the card

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as S from './styled';
+
+const trimText = text => `${text.substring(0, 30)}...`;
+const Card = ({ item, lang }) => {
+  return (
+    <div style={{ width: 300, padding: 30 }}>
+      <S.CardContainer>
+        <S.ImageContainer>
+          <S.Img src={item.image} />
+        </S.ImageContainer>
+        <div>
+          <S.Title>{lang === 'en' ? item.title : item.titleAr}</S.Title>
+          <S.Body>{trimText(lang === 'en' ? item.description : item.descriptionAr)}</S.Body>
+        </div>
+      </S.CardContainer>
+    </div>
+  );
+};
+
+export default Card;
