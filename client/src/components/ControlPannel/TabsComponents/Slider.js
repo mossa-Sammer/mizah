1265975c3fb49d80dc10fce8e822a0bfe142f53d
fadeomar <fadeomar2015@gmail.com>
2020-08-15 22:39:06 +0300

    connect slider page to the backend

--- client/src/components/ControlPannel/TabsComponents/Slider.js
+++ client/src/components/ControlPannel/TabsComponents/Slider.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { withStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import FormLayout from '../FormLayout';
 import { BtnContainer, TitleContainer } from './styled';
 
-import Table from '../../Table';
+import Table from '../../Table'; 
 import { RemoveRejoinCol } from '../../Table/tableSharedData';
 
-const rowClick = () => console.log('Row has been clicked');
+
 
 const style = {
   title: {
@@ -18,31 +19,32 @@ const style = {
   },
 };
 
-export const earnGroupMembersData = [
-  {
-    title: 'Jackie Jensen',
-    titleAr: 'sss Jenssssen',
-    description: 'sss Jenssssen',
-    descriptionAr: 'sss Jenssssen',
-    btnName: 'sss Jenssddssen',
-    btnNameAR: 'sss Jenssddssen',
-  },
-  {
-    title: 'Jackie Jensen',
-    titleAr: 'sss Jenssssen',
-    description: 'sss Jenssssen',
-    descriptionAr: 'sss Jenssssen',
-    btnName: 'sss Jenssddssen',
-    btnNameAR: 'sss Jenssddssen',
-  },
-];
-
 const Slider = ({ classes }) => {
   const [openForm, setOpenForm] = useState(false);
+  const [data, setData] = useState(null)
+  const route = '/api/v1/slider';
 
-  useEffect(() => {
-    // here should get the data when openForm state change
-  }, [openForm]);
+  const rowClick = async (rowData, route) => {
+    try{
+      setData(old => old.filter(e => e.slider_id !== rowData.slider_id))
+      const data = await axios.delete(`${route}/${rowData.slider_id}`)
+    } catch(e){
+      console.log(e)
+    }
+  }
+
+  useEffect( () => {
+  (async () => {
+    try {
+    
+    const data = await axios.get('/api/v1/slider')
+    setData(data.data)
+  } catch(e) {
+    console.log(e)
+  }
+  })()
+
+  }, [openForm])
 
   return (
     <div style={{ paddingLeft: 320 }}>
@@ -61,23 +63,24 @@ const Slider = ({ classes }) => {
         </BtnContainer>
       </TitleContainer>
       {openForm ? (
-        <FormLayout page="slider" />
+
+        <FormLayout page="slider" route={route}/>
       ) : (
         <Table
           hideSearch
           color="blue"
-          data={earnGroupMembersData}
+          data={data || []}
           showPagination={false}
           columns={[
             { title: 'Title', field: 'title' },
-            { title: 'Title AR', field: 'titleAr' },
+            { title: 'Title AR', field: 'title_ar' },
             { title: 'Description', field: 'description' },
-            { title: 'Description AR', field: 'descriptionAr' },
-            { title: 'Btn Name', field: 'btnName' },
-            { title: 'Btn Name AR', field: 'btnNameAR' },
+            { title: 'Description AR', field: 'description_ar' },
+            { title: 'Btn Name', field: 'btn_name' },
+            { title: 'Btn Name AR', field: 'btn_name_ar' },
             RemoveRejoinCol,
           ]}
-          onRowClick={rowClick}
+          onRowClick={(e, rowData) => rowClick(rowData, route)}
         />
       )}
     </div>
