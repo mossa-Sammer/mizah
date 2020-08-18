ccd590859051de1110b04e60ef1f2c9c6a682d4f
fadeomar <fadeomar2015@gmail.com>
2020-08-18 17:31:39 +0300

    add dummy data for the whyus section

---
+++ client/src/Sections/WhyUsSection/dummyData.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    title: 'Continuous technical...',
+    content:
+      'We always provide excellent technical support to solve all problems immediately and to answer all...',
+    titleAr: 'دعم فني مستمر',
+    contentAr:
+      'نقدم ​​دائمًا دعمًا فنيًا ممتازًا ومستمرًا لحل جميع المشكلات على الفور والإجابة على أسئلة العملاء',
+  },
+  {
+    title: 'Cooperation with cli...',
+    content: 'We share ideas and make up strategies with our clients',
+    titleAr: 'التعاون مع العملاء',
+    contentAr: 'نحن نتبادل الأفكار مع عملائنا ونصوغ استراتيجياتنا معًا',
+  },
+  {
+    title: 'Experience',
+    content: 'The stuff consists of experts with more than ten years of experience in this field .',
+    titleAr: 'الخبرة',
+    contentAr:
+      'يتكون فريق العمل لدينا من خبراء يمتلكون في الغالب عشر سنوات أو أكثر من الخبرة في هذا المجال',
+  },
+  {
+    title: 'Integrated marketing...',
+    content:
+      'We provide our clients with marketing plans and solutions for their projects to help them make profi...',
+    titleAr: 'حلول تسويقية متكاملة',
+    contentAr: 'نقدم لعملائنا حلول وخطط تسويقية لمشاريعهم للوصول إلى المستخدم وتحقيق الأرباح',
+  },
+];
+
+export const title = {
+  ar: 'في ميزة نتفهم احتياجاتك ونضمن لك دعمًا إلكترونيًا يجعلك تحقق أرباحاً عالية',
+  en:
+    'In Mizah, we understand what you want and guarantee you an electronic presence that will achieve your high profits',
+};
+export default data;
