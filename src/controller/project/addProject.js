cae18e6d44b9393ed4c833a51166ca960e8a7990
mossa-sammer <mossa708496@gmail.com>
2020-08-18 22:39:58 +0300

    refactored the add project route to add multiple images

--- src/controller/project/addProject.js
+++ src/controller/project/addProject.js
@@ -1,7 +1,8 @@
 const { project } = require('../../database/queries');
+const { addImage } = require('../images');
 
 module.exports = async (req, res, next) => {
-  const { title, titleAr, description, descriptionAr, imageUrl } = req.body;
+  const { title, titleAr, description, descriptionAr, images } = req.body;
   try {
     const {
       rows: [addedProject],
@@ -10,9 +11,23 @@ module.exports = async (req, res, next) => {
       titleAr,
       description,
       descriptionAr,
-      imageUrl,
     });
-    res.json({ msg: 'project added successfully', addedProject });
+    console.log(req.body);
+
+    console.log(title, titleAr, description, descriptionAr, addedProject);
+    let addedImages = [];
+    if (images) {
+      const { rows } = await project.addProjectImages(
+        addedProject.project_id,
+        images
+      );
+      addedImages = [...rows];
+    }
+
+    res.json({
+      msg: 'project added successfully',
+      data: { ...addedProject, images: addedImages },
+    });
   } catch (e) {
     next(e);
   }
