8b27eaecc86a2516af104faa687b2de64bd8186d
mossa-sammer <mossa708496@gmail.com>
2020-08-18 22:38:13 +0300

    made a query to add multiple images

--- src/database/queries/projects/addProject.js
+++ src/database/queries/projects/addProject.js
@@ -1,9 +1,19 @@
 const connection = require('../../config/connection');
+const multiRowInsert = require('../helper/multiRowInsert');
 
-const addProject = ({ title, titleAr, description, descriptionAr, imageUrl }) =>
+const addProject = ({ title, titleAr, description, descriptionAr }) =>
   connection.query(
-    'INSERT INTO project (title,title_ar,description,description_ar,image_url) VALUES ($1,$2,$3,$4,$5) RETURNING *',
-    [title, titleAr, description, descriptionAr, imageUrl]
+    'INSERT INTO project (title,title_ar,description,description_ar) VALUES ($1,$2,$3,$4) RETURNING *',
+    [title, titleAr, description, descriptionAr]
   );
 
-module.exports = addProject;
+const addProjectImages = (projectId, images) => {
+  console.log(11111111111111, images);
+  const { preparedStatment, values } = multiRowInsert(images, projectId);
+  return connection.query(
+    `INSERT INTO project_image (project_id, image) VALUES ${preparedStatment} RETURNING *`,
+    values
+  );
+};
+
+module.exports = { addProject, addProjectImages };
