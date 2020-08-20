d1d24daa58fbdf4ffaf9dc213af45ffe1e665ed8
fadeomar <fadeomar2015@gmail.com>
2020-08-20 21:13:42 +0300

    add clikc event for the project card

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import * as S from './styled';
 
 const ProjectCard = ({ item, lang }) => {
+  const history = useHistory();
   return (
-    <S.Container className="container" bg={item.image}>
+    <S.Container
+      className="container"
+      bg={item.image}
+      onClick={() => history.push(`/project/${item.id}`)}
+    >
       <S.Overlay className="overlay">
         <S.Items className="items" />
         <S.ItemsHead className="items head">
