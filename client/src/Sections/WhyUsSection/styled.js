542c9a8ee021e58a1949dcafbce5b1250d3adfc1
fadeomar <fadeomar2015@gmail.com>
2020-08-20 21:09:15 +0300

    add style for the whyUs section

--- client/src/Sections/WhyUsSection/styled.js
+++ client/src/Sections/WhyUsSection/styled.js
@@ -26,16 +26,14 @@ export const SecSection = styled.div`
   margin-top: 30px;
 `;
 
-export const TitleContainer = styled.div`
-  width: 70%;
-  padding: 0 50px;
+export const MainTitleContainer = styled.div`
   display: flex;
   align-items: ${({ lang }) => (lang === 'en' ? 'flex-start' : 'flex-end')};
   flex-direction: column;
 `;
 
 export const Title = styled.h2`
-  font-size: 25px;
+  font-size: 32px;
   color: #8368af;
   text-align: ${({ lang }) => (lang === 'en' ? 'left' : 'right')};
 `;
@@ -47,28 +45,61 @@ export const TitleSpan = styled.span`
   margin: 0;
 `;
 
+// export const SectionTitle = styled.div`
+//   width: 100%;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: #f5ebfe;
+// `;
+
+// export const SectionTitleText = styled.h1`
+//   font-size: 54px;
+//   color: #52387f;
+//   margin:0;
+//   padding 0 10px;
+// `;
+
 export const SectionTitle = styled.div`
-  width: 100%;
   display: flex;
-  justify-content: center;
+  padding-right: 60px;
+  width: 100%;
+  flex-direction: ${({ lang }) => (lang === 'en' ? 'row' : ' row-reverse')};
   align-items: center;
-  background-color: #f5ebfe;
 `;
 
-export const SectionTitleText = styled.h1`
+export const TitleSubContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 60px;
+`;
+
+export const TitleText = styled.h1`
   font-size: 54px;
   color: #52387f;
   margin:0;
   padding 0 10px;
+  @media (max-width: 960px) {
+    font-size: 38px;
+  }
+  @media (max-width: 420px) {
+    font-size: 26px;
+  }
 `;
 
-export const SecondSecItem = styled.div`
+export const Card = styled.div`
   background-color: #f5ebfe;
   width: 100%;
-  height: 100px;
+  padding: 20px 10px;
   margin-top: 30px;
   border-radius: 15px;
   box-shadow: 5px 10px 20px -8px #000;
+  &:hover {
+    transform: translateY(-5px);
+  }
+  transition: 0.3s;
 `;
 
 export const SubTitle = styled.h5`
@@ -78,6 +109,7 @@ export const SubTitle = styled.h5`
   padding-bottom: 0;
   margin-top: 0;
   color: #8368af;
+  font-size: 20px;
 `;
 
 export const SubTitleSpan = styled.span`
@@ -88,7 +120,7 @@ export const SubTitleSpan = styled.span`
   margin: 0;
 `;
 
-export const SubTitleContainer = styled.div`
+export const ItemTitleContainer = styled.div`
   padding: 0 20px;
   display: flex;
   align-items: ${({ lang }) => (lang === 'en' ? 'flex-start' : 'flex-end')};
@@ -102,4 +134,5 @@ export const TextContent = styled.p`
   padding-bottom: 0;
   margin-top: 0;
   color: gray;
+  font-size: 18px;
 `;
