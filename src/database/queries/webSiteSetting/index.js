b70b4593d6aceff187b7c17d8bcf8b06597e3f9a
mossa-sammer <mossa708496@gmail.com>
2020-09-04 19:21:39 +0300

    fixed bug

--- src/database/queries/webSiteSetting/index.js
+++ src/database/queries/webSiteSetting/index.js
@@ -57,7 +57,7 @@ const editAboutUs = ({
   ourVisionAr,
 }) =>
   connection.query(
-    'UPDATE website SET about_us_description=$1,about_us_description_ar=$2,video_Url=$3, our_values=$4,our_values_ar=$5,our_mission=$6,our_mission_ar=$7, our_vision=$8, our_vision_ar=9 WHERE site_id=$10',
+    'UPDATE website SET about_us_description=$1,about_us_description_ar=$2,video_Url=$3, our_values=$4,our_values_ar=$5,our_mission=$6,our_mission_ar=$7, our_vision=$8, our_vision_ar=$9 WHERE site_id=$10',
     [
       description,
       descriptionAr,
