eb4cf210694a998ff48ffcc2dd8d7ca8e2f5559a
fadeomar <fadeomar2015@gmail.com>
2020-09-05 04:01:05 +0300

    add condition to render the projects

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -5,7 +5,8 @@ import * as S from './styled';
 const ProjectCard = ({ item, lang }) => {
   const history = useHistory();
   return (
-    <S.Container
+    item && item.project_images[0] && item.project_images.image_url && 
+      <S.Container
       className="container"
       bg={item.project_images[0].image_url}
       onClick={() => history.push(`/project/${item.project_id}`)}
