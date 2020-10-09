903ece0f3c4a32e119fdd3b5a5938fe1ae64f9a7
mossa-sammer <mossa708496@gmail.com>
2020-10-09 18:25:46 +0300

    changed bucket name

--- src/config/awsS3.js
+++ src/config/awsS3.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 
-const storageUrl = 'https://myfirstbucket512.s3.eu-west-3.amazonaws.com';
-const bucket = 'myfirstbucket512';
+const bucket = 'mizha-bucket';
 const region = 'eu-west-3';
+const storageUrl = `https://${bucket}.s3.${region}.amazonaws.com`;
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.ACCESS_KEY_ID,
