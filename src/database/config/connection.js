3293ab0e88b0b1e14c52eb25d030b4a41b4897b3
fadeomar <fadeomar2015@gmail.com>
2020-10-24 23:19:59 +0300

    add remote database url

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -1,13 +1,16 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-let dbURL = '';
+let dbURL = "";
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   dbURL = process.env.DATABASE_URL;
-} else if (process.env.NODE_ENV === 'test') {
-  dbURL = '';
+} else if (process.env.NODE_ENV === "test") {
+  dbURL = "";
 } else {
   dbURL = process.env.DEV_DB;
 }
 
-module.exports = new Pool({ connectionString: dbURL });
+module.exports = new Pool({
+  connectionString: dbURL,
+  ssl: { rejectUnauthorized: false },
+});
